4e6c5d4f2f476ab5cc638cecd407f6892001018a
Zé Rodrigues <javr.rodrigues@gmail.com>
2019-10-24 16:29:51 +0100

    Chore( Core ): Add migrations

---
+++ database/migrations/2019_10_24_145336_create_database.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDatabase extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('superheros', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('realName');
+            $table->string('heroName')->unique();
+            $table->string('publisher');
+            $table->date('firstAppearance')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('abilities', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->unique();
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        Schema::create('teams', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->unique();
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        Schema::create('superhero_abilities', function (Blueprint $table) {
+            $table->uuid('idSuperhero');
+            $table->uuid('idAbility');
+
+            $table->foreign('idSuperhero')->references('id')->on('superheros');
+            $table->foreign('idAbility')->references('id')->on('abilities');
+        });
+
+        Schema::create('team_affiliations', function (Blueprint $table) {
+            $table->uuid('idTeam');
+            $table->uuid('idSuperhero');
+
+            $table->foreign('idTeam')->references('id')->on('teams');
+            $table->foreign('idSuperhero')->references('id')->on('superheros');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('team_affiliations');
+        Schema::dropIfExists('superhero_abilities');
+        Schema::dropIfExists('teams');
+        Schema::dropIfExists('abilities');
+        Schema::dropIfExists('superheros');
+    }
+}
